3b94dc4d11029b668bfb2058847effcd640ae1e4
jishnubr <jishnubr2014@gmail.com>
2024-10-12 21:45:22 +0530

    Add internet connectivity check to TimerTriggerJava1 function.

--- src/main/java/com/function/TimerTriggerJava1.java
+++ src/main/java/com/function/TimerTriggerJava1.java
@@ -1,6 +1,8 @@
 package com.function;
 
 import java.time.*;
+import java.net.*;
+import java.io.*;
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
 
@@ -17,5 +19,20 @@ public class TimerTriggerJava1 {
         final ExecutionContext context
     ) {
         context.getLogger().info("Java Timer trigger function executed at: " + LocalDateTime.now());
+
+        try {
+            URL url = new URL("http://www.google.com");
+            HttpURLConnection urlConnect = (HttpURLConnection) url.openConnection();
+            urlConnect.setConnectTimeout(10000); // Set timeout
+            urlConnect.connect();
+
+            if (urlConnect.getResponseCode() == 200) {
+                context.getLogger().info("Internet is available.");
+            } else {
+                context.getLogger().info("Failed to connect to the internet.");
+            }
+        } catch (Exception e) {
+            context.getLogger().severe("Error checking internet connectivity: " + e.getMessage());
+        }
     }
 }
