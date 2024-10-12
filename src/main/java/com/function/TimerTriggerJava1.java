d7ac48581e7c3a121ecc6f2254e265ec0b269669
jishnubr <jishnubr2014@gmail.com>
2024-10-12 21:58:00 +0530

    Need 1 minute triggering

--- src/main/java/com/function/TimerTriggerJava1.java
+++ src/main/java/com/function/TimerTriggerJava1.java
@@ -9,7 +9,7 @@ import com.microsoft.azure.functions.*;
 public class TimerTriggerJava1 {
     @FunctionName("TimerTriggerJava1")
     public void run(
-        @TimerTrigger(name = "timerInfo", schedule = "0 */10 * * * *") String timerInfo,
+        @TimerTrigger(name = "timerInfo", schedule = "0 */1 * * * *") String timerInfo,
         final ExecutionContext context
     ) {
         context.getLogger().info("Java Timer trigger function executed at: " + LocalDateTime.now());
