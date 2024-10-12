d7da19b90f8663081694d2f67ed2824c4e7d2727
jishnubr <jishnubr2014@gmail.com>
2024-10-12 22:09:54 +0530

    Enhance DummyWeatherData with additional weather parameters.

--- src/main/java/com/function/DummyWeatherData.java
+++ src/main/java/com/function/DummyWeatherData.java
@@ -3,15 +3,26 @@ package com.function;
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
 import java.util.Optional;
+
 public class DummyWeatherData {
     @FunctionName("GetDummyWeatherData")
     public HttpResponseMessage get(
         @HttpTrigger(name = "req", methods = {HttpMethod.GET}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
         final ExecutionContext context) {
-        
-        String dummyData = "{\"location\": \"London\", \"temperature\": 20, \"status\": \"Sunny\"}";
+
+        String dummyData = "{"
+                         + "\"location\": \"London\","
+                         + "\"temperature\": 20,"
+                         + "\"humidity\": 65,"
+                         + "\"wind_speed\": 5,"
+                         + "\"wind_direction\": \"NW\","
+                         + "\"pressure\": 1015,"
+                         + "\"precipitation\": 0,"
+                         + "\"visibility\": 10,"
+                         + "\"status\": \"Sunny\""
+                         + "}";
         context.getLogger().info("Returning dummy weather data: " + dummyData);
-        
+
         return request.createResponseBuilder(HttpStatus.OK)
                       .header("Content-Type", "application/json")
                       .body(dummyData)
